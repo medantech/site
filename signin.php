a99d4f458ffb03ba30bad10b85d4f7a9b0a64e71
Medan Tech <46564820+medantech@users.noreply.github.com>
2021-03-02 14:21:41 +0700

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+  <div class="title-group">
+    <h1 class="title">Customer Accounts</h1>
+    <p class="desc">Sign in to the customer dashboard. Always free!</p>
+  </div>
+  <form action="#" method="post">
+    <input type="text" class="input-control" name="user" placeholder="Username or Email Address">
+    <input type="password" class="input-control" name="pass" placeholder="Password">
+    <a href="#" class="btn-link">Forgoten Password?</a>
+    <button class="btn">Sign in</button>
+  </form>
+</div>
+</body>
+</html>
